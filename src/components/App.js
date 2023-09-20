35e729535b9f4511a80f8151cb75f6d3e2464dca
Nitik sharma <sharmanitik36@gmail.com>
2023-09-21 00:08:28 +0530

    submitting code-Thu Sep 21 2023 00:08:26 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+const [input,setInput]=useState('');
+
+
+const [get,setGet]=useState([]);
+
+
+useEffect(() => {
+  const filterData = fruits.filter((item) =>
+    item.toLowerCase().includes(input.toLowerCase())
+  );
+  setGet(filterData);
+}, [input]);
+
+console.log(get);
+  const handleChange=(e)=>{
+     setInput(e.target.value);
+  }
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
+   
+    <div className="container">
+       <h1>Search item</h1>
+       <input type="text" onChange={handleChange} value={input}/>
+       {
+        get.map((item,index)=>(
+          <li key={index}>{item}</li>
+        ))
+       }
     </div>
   )
 }
